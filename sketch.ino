2c2be1d6a0302b8c3e38a992770078b8160cbd77
Joe Kriefall <joekrie@users.noreply.github.com>
2016-12-14 23:23:43 -0600

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -10,6 +10,8 @@ unsigned int sample;
 void setup() {
   lcd.begin(16, 2);
   Wire.begin();
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
 void loop() {
@@ -37,6 +39,14 @@ void loop() {
 
   peakToPeak = signalMax - signalMin;
 
+  if (peakToPeak > 400) {
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+  } else {
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+  }
+
   int transmitValue = 2048 - (int)(peakToPeak * 2);
   Wire.beginTransmission(0x60);
   Wire.write(64);
@@ -49,3 +59,4 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print(peakToPeak);
 }
+

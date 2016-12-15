8eec7f6cbd124dcbfb45aca2d05398f523ee7222
Joe Kriefall <joekrie@users.noreply.github.com>
2016-12-14 23:11:32 -0600

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -15,38 +15,37 @@ void setup() {
 void loop() {
   unsigned long startMillis = millis();
   unsigned int peakToPeak = 0;
-  
+
   unsigned int signalMax = 0;
   unsigned int signalMin = 1024;
-  
+
   while (millis() - startMillis < sampleWindow) {
     sample = analogRead(0);
-    
+
     if (sample < 1024)
     {
-       if (sample > signalMax)
-       {
-          signalMax = sample;
-       }
-       else if (sample < signalMin)
-       {
-          signalMin = sample;
-       }
+      if (sample > signalMax)
+      {
+        signalMax = sample;
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;
+      }
     }
   }
-  
-  peakToPeak = signalMax - signalMin;
-  double volts = (peakToPeak * 5.0) / 1024;
 
-  int transmitValue = (int)(peakToPeak * 20);  
+  peakToPeak = signalMax - signalMin;
+
+  int transmitValue = 2048 - (int)(peakToPeak * 2);
   Wire.beginTransmission(0x60);
   Wire.write(64);
-  Wire.write(transmitValue >> 4);
-  Wire.write((transmitValue & 15) << 4);
+  Wire.write(transmitValue / 16);
+  Wire.write((transmitValue % 16) << 4);
   Wire.endTransmission();
-  
-  lcd.setCursor(0, 0);  
-  lcd.print("        ");  
-  lcd.setCursor(0, 0);  
+
+  lcd.setCursor(0, 0);
+  lcd.print("        ");
+  lcd.setCursor(0, 0);
   lcd.print(peakToPeak);
 }
